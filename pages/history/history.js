1d6a495ff17d1b778cd42969c673a36728f7ba75
jiaforios <admina@bindeiMac.local>
2017-09-26 16:39:48 +0800

    加入历史界面图表库支持

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,4 +1,7 @@
 // pages/history/history.js
+var wxCharts = require('../../vender/charts/wxcharts.js');
+var app = getApp();
+var lineChart = null;
 Page({
 
   /**
@@ -7,12 +10,76 @@ Page({
   data: {
   
   },
-
+  touchHandler: function (e) {
+    lineChart.scrollStart(e);
+  },
+  moveHandler: function (e) {
+    lineChart.scroll(e);
+  },
+  touchEndHandler: function (e) {
+    lineChart.scrollEnd(e);
+    lineChart.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  }, 
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    for (var i = 0; i < 20; i++) {
+      categories.push('2016-' + (i + 1));
+      data.push(Math.random() * (20 - 10) + 10);
+    }
+    // data[4] = null;
+    return {
+      categories: categories,
+      data: data
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var windowWidth = 375;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var simulationData = this.createSimulationData();
+
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+      series: [{
+        name: '成交量1',
+        data: [0.15, 0.2, 0.45, 0.37, 0.4, 0.8, 0.2, 0.45, 0.37, 0.4, 0.8],
+        format: function (val) {
+          return val.toFixed(2) + '万';
+        }
+      }, {
+        name: '成交量2',
+        data: [0.30, 0.37, 0.65, 0.78, 0.69, 0.94, 0.37, 0.65, 0.78, 0.69, 0.94],
+        format: function (val) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      yAxis: {
+        title: '成交金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      enableScroll: true,
+      width: 320,
+      height: 200
+    });
+
   },
 
   /**
