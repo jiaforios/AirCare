4b74e0649f72dce653de4cd4b2e5eff6d1ccfe80
jiaforios <admina@bindeiMac.local>
2017-10-10 16:48:27 +0800

    历史界面显示数据

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,5 +1,6 @@
 // pages/history/history.js
 var wxCharts = require('../../vender/charts/wxcharts.js');
+var util = require('../../utils/util.js');  
 var app = getApp();
 var lineChart = null;
 Page({
@@ -8,7 +9,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      dataArray:null,
+      out_dataArray: null,
+      inDeviceDataArr:null,
+      outDeviceDataArr:null,
+      deviceDataTimeArr:null,
   },
   touchHandler: function (e) {
     lineChart.scrollStart(e);
@@ -41,45 +46,149 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var windowWidth = 375;
-    try {
-      var res = wx.getSystemInfoSync();
-      windowWidth = res.windowWidth;
-    } catch (e) {
-      console.error('getSystemInfoSync failed!');
-    }
+      var that = this;
 
-    var simulationData = this.createSimulationData();
+      var windowWidth = 375;
+      try {
+        var res = wx.getSystemInfoSync();
+        windowWidth = res.windowWidth;
+      } catch (e) {
+        console.error('getSystemInfoSync failed!');
+      }
 
-    lineChart = new wxCharts({
-      canvasId: 'lineCanvas',
-      type: 'line',
-      categories: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
-      series: [{
-        name: '成交量1',
-        data: [0.15, 0.2, 0.45, 0.37, 0.4, 0.8, 0.2, 0.45, 0.37, 0.4, 0.8],
-        format: function (val) {
-          return val.toFixed(2) + '万';
-        }
-      }, {
-        name: '成交量2',
-        data: [0.30, 0.37, 0.65, 0.78, 0.69, 0.94, 0.37, 0.65, 0.78, 0.69, 0.94],
-        format: function (val) {
-          return val.toFixed(2) + '万';
-        }
-      }],
-      yAxis: {
-        title: '成交金额 (万元)',
-        format: function (val) {
-          return val.toFixed(2);
+      var dataArrin = that.data.inDeviceDataArr;
+      lineChart = new wxCharts({
+        canvasId: 'lineCanvas',
+        type: 'line',
+        categories: [0,0],
+        series: [
+          {
+            name: '室内',
+            data: [0,0],
+          }
+
+        ],
+        yAxis: {
+          title: '室内pm2.5 (ug/m3)',
+          // format: function (val) {
+          //   return val.toFixed(2);
+          // },
+          min: 0
         },
-        min: 0
-      },
-      enableScroll: true,
-      width: 320,
-      height: 200
-    });
+        enableScroll: true,
+        width: windowWidth,
+        height: 400
+      });  
 
+
+
+      var eTime = util.formatTime(new Date()); 
+      var baseTime = eTime.substring(0,10);
+      var sTime = baseTime + " " +'00:00:00';
+      var in_deviceid = app.globalData.inDeviceId;
+      var out_deviceid = app.globalData.outDeviceId;
+      var inUrlStr = 'https://www.airmoniter.com/airQuality/queryAirQualitys.do?deviceId=' + in_deviceid + '&' + 'startDate=' + sTime + '&' + 'endDate=' + eTime
+      var outUrlStr = 'https://www.airmoniter.com/airQuality/queryAirQualitys.do?deviceId=' + out_deviceid + '&' + 'startDate=' + sTime + '&' + 'endDate=' + eTime
+
+      wx.request({
+        url: inUrlStr,
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          that.setData({
+            dataArray:res.data.data,
+          })
+          var arr = that.data.dataArray
+
+          var inData = [];
+          var inTimes = [];
+          for (var i = 0; i<arr.length; i++){
+            var obj = arr[i];
+            var time = obj.dateTime;
+            var inTime = time.substr(11,5);
+            inData.push(obj.pm2); // 存储pm2.5
+            inTimes.push(inTime); // 存储时间
+          }
+          inTimes = inTimes.reverse();
+          that.setData({
+            inDeviceDataArr: inData,
+            deviceDataTimeArr: inTimes,
+          })
+          var series = null
+          if (that.data.outDeviceDataArr) {
+            series = [{
+              name: '室外pm2.5',
+              data: that.data.outDeviceDataArr,
+            }, {
+              name: '室内pm2.5',
+              data: inData,
+            }];
+          } else {
+            series = [{
+              name: '室内pm2.5',
+              data: inData,
+            }];
+          }
+
+          lineChart.updateData({
+            categories: inTimes,
+            series: series
+          });
+
+        }
+      })  
+
+      wx.request({
+        url: outUrlStr,
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          that.setData({
+            out_dataArray: res.data.data,
+          })
+          var arr = that.data.out_dataArray
+
+          var outData = [];
+          var outTimes = [];
+          for (var i = 0; i < arr.length; i++) {
+            var obj = arr[i];
+            var time = obj.dateTime;
+            var outTime = time.substr(11, 5);
+            outData.push(obj.pm2); // 存储pm2.5
+            outTimes.push(outTime); // 存储时间
+          }
+          outTimes = outTimes.reverse();
+          that.setData({
+            outDeviceDataArr: outData,
+            deviceDataTimeArr: outTimes,
+          })
+
+          var series = null
+          if (that.data.inDeviceDataArr){
+            series = [{
+              name: '室外pm2.5',
+              data: outData,
+            },{
+                name: '室内pm2.5',
+                data: that.data.inDeviceDataArr,
+            }];
+          }else{
+            series = [{
+              name: '室外pm2.5',
+              data: outData,
+            }];
+          }
+         
+          lineChart.updateData({
+            categories: outTimes,
+            series: series
+          });
+
+        }
+      })  
+      
   },
 
   /**
